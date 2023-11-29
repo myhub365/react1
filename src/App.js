624d1f3800db94ffb45576b065a4c3ed066171a8
myhub365 <nikhil.thedigitalbusiness@gmail.com>
2023-11-29 12:20:46 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   
+   <h1 className='color'>Hello</h1>
+
+   <h1 style={{ color: "red", background: "pink" }}>Hy</h1>
+
+   <input type='text' />
+
+   <button type="" className='btn btn-primary'>Click</button>
+
+   <br />
+
+   <img />
+
+   <hr />
+
+
+   
+   </>
+
+    
   );
 }
 
